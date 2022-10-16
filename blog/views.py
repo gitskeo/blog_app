7412896fd3618356dc2ac84a5e9a500e07e2cf55
gitskeo <skeosouvanh@gmail.com>
2022-10-15 22:31:12 -0500

    added edit, delete, and post features

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,12 @@
+from dataclasses import fields
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import DetailView, ListView
+from django.views.generic import (
+    DetailView,
+    ListView,
+    CreateView,
+    UpdateView,
+    DeleteView,)
 from django.urls import reverse_lazy
 # Create your views here.
 
@@ -13,3 +19,21 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = Post
     template_name = "post_detail.html"
+
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = "post_new.html"
+    fields = ["title", "author", "body"]
+
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = "post_edit.html"
+    fields = ["title", "body"]
+
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = "post_delete.html"
+    success_url = reverse_lazy("home")
